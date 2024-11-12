3a035de689779114e96d3f6620bcab1a97464bbf
Arina Sprogis <sprogis_a@outlook.com>
2024-11-12 20:19:37 +0500

    Fibonacci

--- contest.py
+++ contest.py
@@ -1,22 +1,14 @@
-def comparison(line):
-    substring = ''
-    max_len = 0
-    for symbol in line:
-        if symbol in substring:
-            index = substring.find(symbol)
-            seq = substring[:(index + 1)]
-            substring = substring.lstrip(seq)
-            substring += symbol
-        else:
-            substring += symbol
-            substring_len = len(substring)
-            max_len = max(max_len, substring_len)
-    return max_len
+def count_measurments(number):
+    if number == 0 or number == 1:
+        return 1
+    n = count_measurments(number - 1) + count_measurments(number - 2)
+    return n
 
 
 if __name__ == '__main__':
     with open('input.txt', 'r') as file_in:
         line = file_in.readline().rstrip()
-    result = comparison(line)
+        version = int(line)
+    result = count_measurments(version)
     with open('output.txt', 'w') as file_out:
         file_out.write(str(result))
