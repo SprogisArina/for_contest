de5ecec7b0ae116ecfa8a48036bfac2b3bd263b2
Arina Sprogis <sprogis_a@outlook.com>
2024-11-13 11:00:02 +0500

    Count project

--- contest.py
+++ contest.py
@@ -1,14 +1,16 @@
-def count_measurments(number):
-    if number == 0 or number == 1:
-        return 1
-    n = count_measurments(number - 1) + count_measurments(number - 2)
-    return n
+def count(applicant_number, cycles):
+    applicants = [applicant + 1 for applicant in range(applicant_number)]
+    retired = 0
+    while applicant_number > 0:
+        retired = (retired + cycles - 1) % applicant_number
+        winner = applicants.pop(retired)
+        applicant_number -= 1
+    return winner
 
 
 if __name__ == '__main__':
     with open('input.txt', 'r') as file_in:
-        line = file_in.readline().rstrip()
-        version = int(line)
-    result = count_measurments(version)
+        number, cycles = (int(file_in.readline().rstrip()) for _ in range(2))
+    result = count(number, cycles)
     with open('output.txt', 'w') as file_out:
         file_out.write(str(result))
