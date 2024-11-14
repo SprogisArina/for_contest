aabb782417593dba19bb52a27837b692c00ad618
Arina Sprogis <sprogis_a@outlook.com>
2024-11-14 15:57:23 +0500

    Merge blocks

--- contest.py
+++ contest.py
@@ -1,22 +1,11 @@
-def sort_numbers(n, arr, m, pattern):
-    sorted_arr = []
-    unsorted_arr = arr.copy()
-    for pattern_index in range(m):
-        sample = pattern[pattern_index]
-        for index in range(n):
-            current = unsorted_arr[index]
-            if current == sample:
-                sorted_arr.append(current)
-                arr.remove(current)
-    for i in range(1, len(arr)):
-        current = arr[i]
-        prev = i - 1
-        while prev >= 0 and arr[prev] > current:
-            arr[prev + 1] = arr[prev]
-            prev -= 1
-        arr[prev + 1] = current
-    sorted_arr.extend(arr)
-    return sorted_arr
+def sort_numbers(n, arr):
+    border = 0
+    max_number = 0
+    for index in range(n):
+        max_number = max(max_number, arr[index])
+        if index == max_number:
+            border += 1
+    return border
 
 
 if __name__ == '__main__':
@@ -24,10 +13,5 @@ if __name__ == '__main__':
         n = int(file_in.readline().rstrip())
         arr = [int(number) for number in (
             file_in.readline().rstrip().split(' '))]
-        m = int(file_in.readline().rstrip())
-        pattern = [int(number) for number in (
-            file_in.readline().rstrip().split(' '))]
-    sorted_arr = sort_numbers(n, arr, m, pattern)
-    sorted_arr = [str(number) for number in sorted_arr]
-    result = str.join(' ', sorted_arr)
+    result = sort_numbers(n, arr)
     print(str(result))
